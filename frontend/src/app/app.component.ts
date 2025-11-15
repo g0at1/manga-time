9fe5370fe200a4ff7fe0510725fc090e41216e0f
g0at1 <michal.lendzion@vp.pl>
2025-11-16 00:02:31 +0100

    MT-15: Add notifications

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar.component';
+import { ToastContainerComponent } from './shared/components/toast-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ToastContainerComponent],
   template: `
     <app-navbar></app-navbar>
     <main class="p-4 max-w-4xl mx-auto">
       <router-outlet></router-outlet>
+      <app-toast-container></app-toast-container>
     </main>
   `,
 })
