8ad6b485975181eeabde17f40d983f8a4b249ba0
g0at1 <michal.lendzion@vp.pl>
2025-11-21 17:13:44 +0100

    MT-21: Add translations

--- frontend/src/app/shared/components/navbar.component.ts
+++ frontend/src/app/shared/components/navbar.component.ts
@@ -1,22 +1,34 @@
 import { Component, HostListener } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   isMenuOpen = false;
   isScrolled = false;
+  currentLang = 'en';
 
   constructor(
     private router: Router,
     private authService: AuthService,
-  ) {}
+    private translate: TranslateService,
+  ) {
+    this.translate.addLangs(['en', 'pl']);
+    this.translate.setDefaultLang('en');
+
+    const saved = localStorage.getItem('lang');
+    const initial = saved || 'en';
+
+    this.translate.use(initial);
+    this.currentLang = initial;
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -29,6 +41,13 @@ export class NavbarComponent {
   logout() {
     this.authService.logout();
     this.router.navigate(['/auth/login']);
+  }
+
+  changeLang(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.currentLang = value;
+    this.translate.use(value);
+    localStorage.setItem('lang', value);
   }
 
   @HostListener('window:scroll')
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes), provideHttpClient(withInterceptors([authInterceptor]))],
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    ...TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }).providers!,
+  ],
 }).catch((err) => console.error(err));
